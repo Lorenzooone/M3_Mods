046639880ab0492a0e75c4ce62446b58a362a76a
Lorenzo Carletti <lollo.lollo.rbiz@gmail.com>
2021-07-01 22:14:00 +0200

    Implement dialogue skipping

--- m3hack.asm
+++ m3hack.asm
@@ -13,12 +13,35 @@ org $812030C; dd $9F92600
 org $9F92600; incbin sound_relocate_dump.bin
 
 //============================================================================================
+//                               TEXT SKIPPING SHORTCUT
+//============================================================================================
+
+define shortcut $0201 //L+A
+
+// Make it so the game skips the text printing portion if a specific shortcut is pressed
+// 04 00 32 00
+org $801D70C
+bl   skipping_hacks.text_skipping_dialogue
+cmp  r0,#0
+bne  +
+b    $801D858
++
+// 04 00 33 00
+org $801D874
+bl   skipping_hacks.text_skipping_dialogue
+cmp  r0,#0
+bne  +
+b    $801D9B8
++
+
+
+//============================================================================================
 //                                    NEW HACK CODE
 //============================================================================================
 
 // Now insert the hack code
 org $813C000
-//incsrc general_hacks.asm
+incsrc skipping_hacks.asm
 
 
 
---
+++ skipping_hacks.asm
@@ -0,0 +1,27 @@
+skipping_hacks:
+
+// ==============================================
+// This hack checks if a specific shortcut is being pressed.
+// If it is, skip printing text.
+// ==============================================
+.text_skipping_dialogue:
+push {lr}
+
+ldr  r0,=#0x2018CCA
+ldrh r0,[r0,#0]
+ldr  r1,=#{shortcut}         // Is the shortcut being pressed?
+and  r0,r1
+cmp  r0,r1
+bne  +
+
+mov  r0,#0                   // End the routine with a signal
+b    .text_skipping_dialogue_end
++
+
+mov  r0,r7
+mov  r1,#1
+bl   $8021914
+ldr  r0,=#0x2005C00
+
+.text_skipping_dialogue_end:
+pop  {pc}
