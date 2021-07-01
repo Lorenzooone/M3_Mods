0eb2ed83e08192e9faf50a5425f2345e46042867
Lorenzo Carletti <lollo.lollo.rbiz@gmail.com>
2021-07-01 23:13:22 +0200

    Setup for different patches based on the base rom

--- i.bat
+++ i.bat
@@ -2,14 +2,16 @@
 @echo Copying base ROM (mother3.gba) to new test ROM (test.gba)
 @echo ---------------------------------------------------------
 @echo.
-@copy mother3j.gba test.gba
+@copy mother3j.gba testj.gba
+@copy mother3e.gba teste.gba
 @echo.
 @echo.
 @echo ---------------------------------------------------------
 @echo Compiling .asm files and inserting all new data files
 @echo ---------------------------------------------------------
 @echo.
-@xkas test.gba m3hack.asm
+@xkas testj.gba m3hack.asm
+@xkas teste.gba m3hack.asm
 @echo.
 @echo.
 @echo COMPLETE!
