71e44563df70a144593f5ff2b6bec7174ae03fe2
Lorenzooone <lollo.lollo.rbiz@gmail.com>
2023-05-07 17:02:04 +0200

    Remove language difference

--- i.bat
+++ i.bat
@@ -2,16 +2,14 @@
 @echo Copying base ROM (mother3.gba) to new test ROM (test.gba)
 @echo ---------------------------------------------------------
 @echo.
-@copy mother3j.gba testj.gba
-@copy mother3e.gba teste.gba
+@copy mother3j.gba test.gba
 @echo.
 @echo.
 @echo ---------------------------------------------------------
 @echo Compiling .asm files and inserting all new data files
 @echo ---------------------------------------------------------
 @echo.
-@xkas testj.gba m3hackj.asm
-@xkas teste.gba m3hack.asm
+@xkas test.gba m3hack.asm
 @echo.
 @echo.
 @echo COMPLETE!
